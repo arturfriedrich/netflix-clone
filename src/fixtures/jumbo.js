698f10f787acc16f3922b1f731c67eb8a1c5df0d
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-07 00:13:01 +0200

    Housing our Jumbotron within a Container

---
+++ src/fixtures/jumbo.js
@@ -0,0 +1,31 @@
+import HomeTv from "../images/misc/home-tv.jpg"
+import HomeMobile from "../images/misc/home-mobile.jpg"
+import HomeImac from "../images/misc/home-imac.jpg"
+
+export default [
+    {
+      "id": 1,
+      "title": "Enjoy on your TV.",
+      "subTitle": "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+      "image": HomeTv,
+      "alt": "Tiger King on Netflix",
+      "direction": "row"
+    },
+    {
+      "id": 2,
+      "title": "Download your programmes to watch on the go.",
+      "subTitle": "Save your data and watch all your favourites offline.",
+      "image": HomeMobile,
+      "alt": "Tiger King on Netflix",
+      "direction": "row-reverse"
+    },
+    {
+      "id": 3,
+      "title": "Watch everywhere.",
+      "subTitle": "Stream unlimited films and TV programmes on your phone, tablet, laptop and TV without paying more.",
+      "image": HomeImac,
+      "alt": "Money Heist on Netflix",
+      "direction": "row"
+    }
+  ]
+  

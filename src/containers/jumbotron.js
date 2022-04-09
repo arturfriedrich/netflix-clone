494cb59302fe01298d6965b3a2a16d90737a9059
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-09 15:54:43 +0200

    Component Library Tree Shaking

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import jumboData from "../fixtures/jumbo.js"
-import Jumbotron from "../components/jumbotron"
+import { Jumbotron } from "../components"
 
 export function JumbotronContainer() {
     return (

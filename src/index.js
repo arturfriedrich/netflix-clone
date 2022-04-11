d07da6145d79162616683a492afc44ab1d776b6d
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-11 23:32:21 +0200

    Connecting our Application to Firebase

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom';
 import { GlobalStyles } from "./global-styles"
 import { App } from './app';
 import { FirebaseContext } from './context/firebase'
+import { initializeApp } from "firebase/app"
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyBJ7mTpUYwAexaiGA5ipf34qUJK9cBeBbo",
+    authDomain: "netflix-clone-65dfc.firebaseapp.com",
+    projectId: "netflix-clone-65dfc",
+    storageBucket: "netflix-clone-65dfc.appspot.com",
+    messagingSenderId: "667932565119",
+    appId: "1:667932565119:web:e140a0a3b97e5c2285694a"
 }
 
-const firebase = window.firebase.initializeApp(config)
+const firebase = initializeApp(config)
 
 ReactDOM.render(
     <>

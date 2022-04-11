6b82757c0a0896edf045d289e819e61752327ac6
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-11 22:58:42 +0200

    Building out the Feature Component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { OptForm } from '../components'
+import { OptForm, Feature } from "../components"
 import { FaqsContainer } from "../containers/faqs"
 import { JumbotronContainer } from "../containers/jumbotron"
 import { FooterContainer } from "../containers/footer"
@@ -9,6 +9,10 @@ export default function Home() {
     return (
         <>
             <HeaderContainer>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                </Feature>
                 <OptForm>
                     <OptForm.Input placeholder="Email Address" />
                     <OptForm.Button>Try it now</OptForm.Button>

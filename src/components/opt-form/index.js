39c593ec502dd3d6094b68e87e8a1aef35678e9d
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-11 18:18:34 +0200

    Building the Opt-Form Component

---
+++ src/components/opt-form/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Input, Button, Text } from './styles/opt-form'
+
+import ChevronRight from "/Users/friedrichartur/Development/frontEnd/scrimbaThings/netflix-clone/src/images/icons/chevron-right.png"
+
+export default function OptForm({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+}
+
+OptForm.Input = function OptFormInput({ ...restProps }) {
+    return <Input {...restProps} />
+}
+
+OptForm.Button = function OptFormButton({ children, ...restProps }) {
+    return (
+        <Button {...restProps}>
+            {children} <img src={ChevronRight} alt="Try Now" />
+        </Button>
+    )
+}
+
+OptForm.Text = function OptFormText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>
+}
+
